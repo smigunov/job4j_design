9576248af67111a02e658c0fc266af8bc118814f
srjm <mamonts@gmail.com>
2023-09-29 09:59:06 +0300

    2.2.1 AssertJ.

--- src/test/java/ru/job4j/ArrayItTest.java
+++ src/test/java/ru/job4j/ArrayItTest.java
@@ -24,4 +24,14 @@ class ArrayItTest {
         assertThat(iterator.next()).isEqualTo(2);
         assertThat(iterator.next()).isEqualTo(3);
     }
+
+    @Test
+    void whenReadSequence1() {
+        ArrayIt iterator = new ArrayIt(
+                new int[] {4, 5, 6}
+        );
+        assertThat(iterator.next()).isEqualTo(4);
+        assertThat(iterator.next()).isEqualTo(5);
+        assertThat(iterator.next()).isEqualTo(6);
+    }
 }
