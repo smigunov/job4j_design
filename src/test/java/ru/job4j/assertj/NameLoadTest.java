f5ee5c3471ad4b48a0afd1376b884c0285ccbd6d
srjm <mamonts@gmail.com>
2023-09-29 23:21:42 +0300

    2.2.1 AssertJ. Chapter 4. Утверждения с исключениями

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkName() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("12345")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("12345");
+    }
+
+    @Test
+    void checkKey() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("=12345")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a key");
+    }
+
+    @Test
+    void checkValue() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("12345=")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a value");
+    }
+}
