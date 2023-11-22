3bce09b54b77f2e9ec6a017919873c256c49aeee
srjm <mamonts@gmail.com>
2023-11-22 22:16:28 +0300

    2.2.1 Ввыод-вывод. 5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -8,14 +8,16 @@ import java.util.stream.Collectors;
 
 public class LogFilter {
     private final String file;
+    List<String> filteredLst;
 
     public LogFilter(String file) {
         this.file = file;
+        this.filteredLst = filter();
     }
 
     public List<String> filter() {
         List<String> result = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader("data/log.txt"))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(this.file))) {
             result = in.lines().filter(s -> s.matches("^.*404$")).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
@@ -23,8 +25,16 @@ public class LogFilter {
         return result;
     }
 
+    public void saveTo(String out) {
+        try (PrintWriter output = new PrintWriter(out)) {
+            this.filteredLst.forEach(output::println);
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter("data/log.txt");
-        logFilter.filter().forEach(System.out::println);
+        new LogFilter("data/log.txt").saveTo("data/404.txt");
     }
 }
