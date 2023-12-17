3d9cda36bd1fb1be33ce5ae4572cbf030f52f4d7
srjm <mamonts@gmail.com>
2023-12-17 15:41:10 +0300

    2.2.1 Ввыод-вывод. 12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,19 +1,27 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("Extension is null");
+        }
+        File file = new File(args[0]);
+        String extension = args[1];
+
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("size : %s", file.getTotalSpace()));
-        for (File subfile : file.listFiles()) {
-            System.out.printf("File name = %s ; File size = %d \r\n", subfile.getName(), subfile.length());
-        }
+        Arrays.stream(file.listFiles()).filter(x -> x.getName().endsWith(extension)).forEach(x -> {
+            System.out.printf("File name = %s ; File size = %d \r\n", x.getName(), x.length());
+        });
     }
 }
