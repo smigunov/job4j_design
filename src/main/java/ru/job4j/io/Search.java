c37b38ea715880b6ed00bbcf7e809813955bfd36
srjm <mamonts@gmail.com>
2023-12-18 20:42:41 +0300

    2.2.1 Ввыод-вывод. 12. Валидация параметров запуска [#246865 #467030]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,15 +9,29 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
-        }
-        if (args.length == 1) {
-            throw new IllegalArgumentException("Extension is null");
-        }
+        validate(args);
         Path start = Paths.get(args[0]);
         String extension = args[1];
         search(start, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
+    }
+
+    public static void validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("There should be 2 arguments");
+        }
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is not specified");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("Extension is not specified");
+        }
+        Path path = Paths.get(args[0]);
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Invalid path");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Invalid extension");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
